70c29ea2f062fe3cf15274dab3c768e1855e5f62
Romzaks <112806167+Romzaks@users.noreply.github.com>
2023-04-13 21:56:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from hyppo.ksample import Energy, MMD
+from statsmodels.distributions.empirical_distribution import ECDF
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 333357078 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.08
+    pl = lambda x, y: MMD(compute_kernel="rbf", gamma=1/10).test(x, y)[1]
+    p = pl(x, y)
+    if p <= alpha:
+        return True
+    else:
+        return False
